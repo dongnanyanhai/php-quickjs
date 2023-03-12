5b4ed3e60ae6e7a5a44f63363e98447d085f9bef
阿海 <978868928@qq.com>
2023-03-12 11:14:38 +0800

    放弃项目，受限于php ffi的能力，无法实现目标。

--- test/qjsffi.php
+++ test/qjsffi.php
@@ -8,7 +8,7 @@ $callback = function($name,$num){
 // int *callback(char *funname, ...)
 $ffi = FFI::cdef('typedef void(*callback)(char *funname,int num);
 int php_js_run(char *filename, callback fn, int trace_memory, size_t stack_size, size_t memory_limit);
-', './libffiqjs.dll');
+', '../build/libffiqjs.dll');
 
 $result = $ffi->php_js_run(__DIR__ . '/test.js',$callback, 0, 0, 0);
 
