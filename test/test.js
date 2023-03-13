4c5ef0bf6f95c42a3c401f709a721a668978a6ed
阿海 <978868928@qq.com>
2023-03-13 11:23:34 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
 import * as std from "std";
 
 console.log = function(msg) {
+	if(typeof msg === 'object'){
+		msg = JSON.stringify(msg);
+	}
     bridge('printf', JSON.stringify(["%s\n", msg]), true);
 }
 // var re = bridge('var_dump','by test.js',false);
 // var re =  bridge('file_put_contents','["aaasdfasd.txt","asdfkhsdasdfasdf"]',true);
 // var re = bridge('print','by test.js',false);
 console.log('test console.log');
-// console.log(JSON.stringify);
+// console.log([1,2,4]);
+console.log(globalThis);
 var re = bridge('test_arr_return','',false);
 console.log(re);
